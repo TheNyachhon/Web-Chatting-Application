b34a74d67a28e4517c2f187814d66dc451337ee9
nishit-shah190 <nishitshah190@gmail.com>
2021-09-23 00:11:42 +0545

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import passport from 'passport';
-import mongoose  from 'mongoose';
 import session from 'express-session';
 import PassportLocalMongoose from 'passport-local-mongoose';
 const GoogleStrategy = require ('passport-google-oauth20').Strategy
