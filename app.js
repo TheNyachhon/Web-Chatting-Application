71cbf4245b55b7039e5467bd7de33a9873880721
nishit-shah190 <nishitshah190@gmail.com>
2021-09-27 15:52:22 +0545

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ import bodyParser from 'body-parser';
 import passport from 'passport';
 import session from 'express-session';
 import PassportLocalMongoose from 'passport-local-mongoose';
-const GoogleStrategy = require ('passport-google-oauth20').Strategy
+const GoogleStrategy = require ('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 
 const app = express();
-
-app.use (session ({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized:false
-}));
+app.use(express.urlencoded({extended:true}))
