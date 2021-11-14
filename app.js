94c3e991d301f88012be4630d1b175df31331144
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-11-14 20:51:02 +0530

    ejs added

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require('dotnev').config()
-import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import passport from 'passport';
@@ -8,6 +7,50 @@ import PassportLocalMongoose from 'passport-local-mongoose';
 const GoogleStrategy = require ('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
+// const express = require('express');
+// const path = require('path');
 
 const app = express();
 app.use(express.urlencoded({extended:true}))
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+//view engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'))
+
+//Redirection
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+app.get('/register',(req,res)=>{
+    res.render('register');
+});
+app.get('/home',(req,res)=>{
+    res.render('home');
+});
+app.get('/HamroChat-home',(req,res)=>{
+    res.render('HamroChat-home');
+})
+app.get('/HamroChat-user-profile',(req,res)=>{
+    res.render('HamroChat-user-profile')
+})
+app.get('/HamroChat-message',(req,res)=>{
+    res.render('HamroChat-message')
+})
+app.get('/change-credentials',(req,res)=>{
+    res.render('change-credentials')
+})
+app.get('/contact',(req,res)=>{
+    res.render('contact')
+})
+
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000");
+})
